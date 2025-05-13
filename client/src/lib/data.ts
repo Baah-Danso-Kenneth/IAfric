0f7cec8cfe8c1c227f7a33adf2d7339256c570e0
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-13 19:19:49 +0000

    Add lib with types and images

---
+++ client/src/lib/data.ts
@@ -0,0 +1,40 @@
+export const navLinks=[
+    {name: '2025 trip calender',href:'/calender'},
+    {name:'all experience',href:'/contacts'},
+    {name:'shop', href:'/shop'},
+    {name:'membership',href:'/memebership'},
+    {name:'about',href:'/about'}
+]
+
+export const littleInfoData=[
+    {title:'connect', description:'our trips bring together awesome people around Africa'},
+    {title:'experience', description:'our trips bring together awesome people around Africa'},
+    {title:'celebrate', description:'our trips bring together awesome people around Africa'},
+]
+
+export const whereweGoData=[
+    {name:'south africa', image:'/images/cantropae.jpg'},
+    {name:'kenya', image:'/images/cantropae.jpg'},
+    {name:'nigeria', image:'/images/cantropae.jpg'},
+    {name:'ghana', image:'/images/cantropae.jpg'},
+    {name:'egypt', image:'/images/cantropae.jpg'},
+    {name:'marituius', image:'/images/cantropae.jpg'},
+    {name:'morocco', image:'/images/cantropae.jpg'},
+    {name:'tanzania', image:'/images/cantropae.jpg'},
+
+]
+
+export const testimonials = [
+    {
+      name: "Joana Ashley",
+      testimony: "Going on a trip with Wild Terrains is like having an in-the-know girlfriend arrange your itinerary."
+    },
+    {
+      name: "Theophilus Issah",
+      testimony: "Cannot recommend Wild Terrains—and the Vogue-worthy curation of their trips—enough!"
+    },
+    {
+      name: "Philipa Befi",
+      testimony: "They are truly the most perfectly curated trips - and that is coming from a very selective traveler."
+    }
+  ];

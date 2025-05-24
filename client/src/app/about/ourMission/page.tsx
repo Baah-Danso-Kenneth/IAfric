274a92311814d9abc77f121fe1a01b53ac2bb6bd
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-24 22:21:08 +0000

    Page Skeleton created

---
+++ client/src/app/about/ourMission/page.tsx
@@ -0,0 +1,8 @@
+import OurMission from '@/components/views/ourMission-page'
+import React from 'react'
+
+function Page() {
+  return <OurMission/>
+}
+
+export default Page
---
+++ client/src/app/shop/page.tsx
@@ -0,0 +1,8 @@
+import ShopPage from '@/components/views/shop-page'
+import React from 'react'
+
+function Page() {
+  return <ShopPage/>
+}
+
+export default Page

cec36c4643de346f488abd220cf4b7d96dfbacc5
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-24 22:16:08 +0000

    Create redux for handling state management and fetching data

---
+++ client/src/redux/features/all-experience/allExperieneApi.ts
@@ -0,0 +1,13 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const allExperienceApi = createApi({
+    reducerPath: 'allApi',
+    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_URL}),
+    endpoints: (builder)=>({
+        getExperience: builder.query<any, string>({
+            query:(slug)=>`all-experience/${slug}/`
+        })
+    })
+})
+
+export const {useGetExperienceQuery} = allExperienceApi;

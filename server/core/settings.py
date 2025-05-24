411788bcb0bad6cc88ad6a663f27e9c14f17698d
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-24 22:24:10 +0000

    Ensure media/ is added to get local images through the media folder locally

--- server/core/settings.py
+++ server/core/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from decouple import config
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,6 +123,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files (CSS, JavaScript, Images)

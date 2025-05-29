035dd838cae55efdd69dad2747d7d514a9d12a77
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-29 18:57:09 +0000

    Write serializer to be used over the internet from the backend

--- server/apps/shops/serializers.py
+++ server/apps/shops/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
-from apps.shops.models import ProductImage, Product
+from apps.shops.models import (
+    ProductImage, Product,
+    ProductCategory
+)
 
 
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['id','name','slug']
 
 class ProductImageSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = ProductImage
         fields = [
@@ -16,11 +22,13 @@ class ProductImageSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     images = ProductImageSerializer(many=True, read_only=True)
+    category = ProductCategorySerializer(read_only=True)
 
     class Meta:
         model = Product
         fields = [
             "id", "name", "slug", "price_in_sats",
             "price_in_fiat", "description", "short_description",
-            "is_featured", "is_active","images"
+            "is_featured", "is_active","images",
+            "category",
         ]

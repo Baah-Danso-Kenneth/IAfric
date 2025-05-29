fda315092fd3cc7e1a49e611cf7f4a574e410433
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-29 09:52:32 +0000

    Views for getting all products

--- server/apps/shops/views.py
+++ server/apps/shops/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ProductImage, Product
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from .serializers import ProductSerializer
+
+
+class ShopProductListView(ListAPIView):
+    queryset = Product.objects.filter(is_active=True,
+                                     stock_quantity__gt=0
+                                     ).prefetch_related("images")
+    serializer_class = ProductSerializer

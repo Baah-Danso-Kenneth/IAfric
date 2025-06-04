c4a448e3e3f95bb14d95a0cc4f3400c8caa8fcac
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-06-04 22:25:08 +0000

    Register the payment state into the store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,17 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit'
 import { allExperienceApi } from './features/all-experience/allExperieneApi'
 import { listProductsApi } from './features/list-products/list-products'
+import { paymentsApi } from './features/payment/paymentApi'
 
 
 export const store = configureStore({
     reducer: {
         [allExperienceApi.reducerPath]: allExperienceApi.reducer,
         [listProductsApi.reducerPath]: listProductsApi.reducer,
+        [paymentsApi.reducerPath]: paymentsApi.reducer,
     },
     middleware: (getDefaultMiddleware)=>
         getDefaultMiddleware().concat(
             allExperienceApi.middleware,
             listProductsApi.middleware,
+            paymentsApi.middleware,
         ),
 })
 

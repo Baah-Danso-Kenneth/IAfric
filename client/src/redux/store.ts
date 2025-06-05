3f07034532fffe08e421b221e60c0d5226e86f87
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-06-05 09:03:32 +0000

    Register CartApi into the store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import { allExperienceApi } from './features/all-experience/allExperieneApi'
 import { listProductsApi } from './features/list-products/list-products'
 import { paymentsApi } from './features/payment/paymentApi'
+import { cartApi } from './features/carts/cartsApi'
 
 
 export const store = configureStore({
@@ -9,12 +10,14 @@ export const store = configureStore({
         [allExperienceApi.reducerPath]: allExperienceApi.reducer,
         [listProductsApi.reducerPath]: listProductsApi.reducer,
         [paymentsApi.reducerPath]: paymentsApi.reducer,
+        [cartApi.reducerPath]: cartApi.reducer
     },
     middleware: (getDefaultMiddleware)=>
         getDefaultMiddleware().concat(
             allExperienceApi.middleware,
             listProductsApi.middleware,
             paymentsApi.middleware,
+            cartApi.middleware,
         ),
 })
 

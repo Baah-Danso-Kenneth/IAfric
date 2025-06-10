eb5b6036fdc4aeaec15da41379f87a9178cc4ebf
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-06-10 12:00:01 +0000

    rectify issues

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,9 +9,22 @@ const nextConfig: NextConfig = {
         hostname: '127.0.0.1',
         port:'8000',
         pathname:'/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**'
       }
     ]
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
